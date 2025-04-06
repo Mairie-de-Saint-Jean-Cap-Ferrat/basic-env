c7539dcd4aa7dbe0173c5b2cefc408be82802b26
Y0n1x <fr.may.johann@gmail.com>
2025-04-06 06:22:48 +0000

    fix: update coder provider version to 1.0.0 for compatibility with modules

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     coder = {
       source  = "coder/coder"
-      version = "0.11.0"  # Using an older version compatible with the modules
+      version = "1.0.0"  # Using an fixed version compatible with the modules
     }
 
     docker = {
